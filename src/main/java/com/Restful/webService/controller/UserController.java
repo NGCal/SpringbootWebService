842abb60c83a7b30a3ce4a468c7d5255aceec9fe
nathgcal <nathalia.gcalzado@hotmail.com>
2021-03-23 22:49:20 -0400

    Added endpoint to get all users

--- src/main/java/com/Restful/webService/controller/UserController.java
+++ src/main/java/com/Restful/webService/controller/UserController.java
@@ -1,15 +1,25 @@
 package com.Restful.webService.controller;
 
+import com.Restful.webService.model.User;
+import com.Restful.webService.services.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 public class UserController {
+    @Autowired
+    UserDao users;
 
-    @RequestMapping(value = "/hello-world",method = RequestMethod.GET)
-    @ResponseBody
-    public String helloWorld(){
-        return "Hello World";
+    @GetMapping(path = "/users")
+    public List<User> getAllUsers() {
+        return users.findAll();
     }
 
 }
