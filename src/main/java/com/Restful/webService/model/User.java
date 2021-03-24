2eddf9dcfebaf37cd6b79e3df3dfd479d29685d4
nathgcal <nathalia.gcalzado@hotmail.com>
2021-03-23 21:42:28 -0400

    Created User class

---
+++ src/main/java/com/Restful/webService/model/User.java
@@ -0,0 +1,48 @@
+package com.Restful.webService.model;
+
+import java.util.Date;
+
+public class User {
+    int id;
+    String name;
+    Date birthday;
+
+    public User(int id, String name, Date birthday) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
