c25c2ce5cdd2827d961268b11ebf7d922f988eae
nathgcal <nathalia.gcalzado@hotmail.com>
2021-03-24 00:56:42 -0400

    Created a new Exception format

--- src/main/java/com/Restful/webService/controller/UserController.java
+++ src/main/java/com/Restful/webService/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.Restful.webService.controller;
 
+import com.Restful.webService.exception.UserNotFoundException;
 import com.Restful.webService.model.User;
 import com.Restful.webService.services.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.persistence.PostUpdate;
 import java.net.URI;
 import java.util.List;
 
@@ -24,6 +26,10 @@ public class UserController {
 
     @GetMapping(path = "/users/{id}")
     public User getUser(@PathVariable int id) {
+        User user = users.findById(id);
+        if (user == null) {
+            throw new UserNotFoundException("User id-" + id);
+        }
         return users.findById(id);
     }
 
@@ -37,5 +43,24 @@ public class UserController {
 
         return ResponseEntity.ok(location);
     }
+
+    @PutMapping(path = "/users/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable int id, @RequestBody User user) {
+        user.setId(id);
+        User newUser = users.update(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequestUri().build().toUri();
+
+        return ResponseEntity.ok(location);
+    }
+
+    @DeleteMapping(path = "/users/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable int id) {
+
+        User User = users.remove(id);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequestUri().build().toUri();
+
+        return ResponseEntity.ok(location);
+    }
+
 
 }
